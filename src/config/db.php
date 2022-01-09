d8666ebbcf7b4470ede72422c079b250257c860b
Đỗ Phi Long <94332877+longdp21@users.noreply.github.com>
2022-01-09 09:20:41 +0700

    bai kiem tra

--- src/config/db.php
+++ src/config/db.php
@@ -2,7 +2,7 @@
     const DB_HOST = 'localhost';
     const DB_USERNAME = 'root';
     const DB_PASSWORD = '';
-    const DB_NAME = '';// tên csdl
+    const DB_NAME = '1951060822-employees';
 
 
 ?>
